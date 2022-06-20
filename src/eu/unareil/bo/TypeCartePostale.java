e5d918aa69d2ad2f2bd7b0c05b507767194f870c
tgentreau <gentreau.thomas@gmail.com>
2022-06-20 15:21:01 +0200

    fini tp

---
+++ src/eu/unareil/bo/TypeCartePostale.java
@@ -0,0 +1,11 @@
+package eu.unareil.bo;
+
+public enum TypeCartePostale {
+    Paysage("Paysage");
+
+    private String libelle;
+
+    TypeCartePostale(String libelle) {
+        this.libelle = libelle;
+    }
+}
